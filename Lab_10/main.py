5e768a91d164f7d8629fd65c609cd8b31a01745a
sasha-grajdeanu <alexandru.c.grajdeanu@gmail.com>
2025-05-15 21:34:51 +0300

    format code

--- Lab_10/main.py
+++ Lab_10/main.py
@@ -3,60 +3,91 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import pearsonr
 
-df = pd.read_csv("data/chile.csv")
-
 #exercitiul 1
+def exercise_1(df):
+    print("==============EXERCITIUL 1=================")
+    print("-----head-----")
+    print(df.head())
+    print("-----shape-----")
+    print(df.shape)
+    print("-----info-----")
+    print(df.info())
+    print("-----describe-----")
+    print(df.describe())
+    print("===============================")
 
-print(df.head())
-print(df.shape)
-print(df.info())
-print(df.describe())
+    print("\nEducation\n", df["education"].count())
+    print("Education: \n", df["education"].value_counts())
+    print("----------------------------")
+    print("\nRegion\n", df["region"].count())
+    print("Region: \n", df["region"].value_counts())
+    print("----------------------------")
+    print("\nSex\n", df["sex"].count())
+    print("Sex: \n", df["sex"].value_counts())
+    print("----------------------------")
+    print("\nVote\n", df["vote"].count())
+    print("Vote: \n", df["vote"].value_counts())
+    print("----------------------------")
 
-print("\neducation\n", df["education"].count())
-print(df["education"].value_counts())
-print("vote: \n", df["vote"].value_counts())
+    df["income"].dropna()
+    print(df.groupby("income")["age"].mean())
+    print("groupby:", df.groupby(["education", "vote"])["age"].mean())
+    print("===============================")
 
-df["income"].dropna()
-print(df.groupby("income")["age"].mean())
-
-print("groupby:", df.groupby(["education", "vote"])["age"].mean())
 
 #exercitul 2
+def exercise_2(df):
+    print("===============Exercitiul 2================")
+    print("mean of income ", df["income"].mean())
+    print("var of income ", df["income"].var())
 
-print("mean of income ", df["income"].mean())
-print("var of income ", df["income"].var())
+    print("plot for income")
+    plt.hist(df["income"].dropna())
+    plt.show()
 
-plt.hist(df["income"].dropna())
-plt.show()
+    print("plot for population")
+    num_bins = 10
+    plt.hist(df["population"], bins=num_bins, density=True, facecolor='blue', alpha=0.7)
+    plt.show()
 
-num_bins = 10
-plt.hist(df["population"], bins=num_bins, density=True, facecolor='blue', alpha=0.7)
-plt.show()
+    print("boxplot for income")
+    y = list(df["population"])
+    plt.boxplot(y)
+    plt.show()
+    print("===============================")
 
-y = list(df["population"])
-plt.boxplot(y)
-plt.show()
 
 #exercitiul 3
+def exercise_3(df):
+    print("===============Exercitiul 3================")
+    print("First point")
+    print("Covariance:\n", df[["age", "statusquo"]].cov())
+    print("Correlation:\n", df[["age", "statusquo"]].corr())
 
-print("Covariance:\n", df[["age", "income"]].cov())
-print("Correlation:\n", df[["age", "income"]].corr())
+    plt.figure(figsize=(5, 4))
+    sns.heatmap(df[["age", "statusquo"]].corr(), annot=True, cmap='coolwarm', fmt=".2f")
+    plt.title("Correlation Heatmap between Age and income")
+    plt.show()
 
-sns.heatmap(df[["age", "income"]].corr(), annot=True, cmap='coolwarm')
-plt.title("Correlation Heatmap between Age and income")
-plt.show()
+    print("\nSecond point")
+    aligned_df = df[["age", "statusquo"]].dropna()
+    corr_coef, p_value = pearsonr(aligned_df["age"], aligned_df["statusquo"])
+    print(f"Pearson correlation coefficient: {corr_coef:.4f}")
+    print(f"P-value: {p_value:.4f}")
 
-age = df["age"].dropna()
-income = df["income"].dropna()
-aligned_df = df[["age", "income"]].dropna()
-corr_coef, p_value = pearsonr(aligned_df["age"], aligned_df["income"])
+    print("\nThird point")
 
-print(f"Pearson correlation coefficient: {corr_coef:.4f}")
-print(f"P-value: {p_value:.4f}")
+    plt.figure(figsize=(6, 4))
+    plt.scatter(aligned_df["age"], aligned_df["statusquo"], alpha=0.6)
+    plt.xlabel("Age")
+    plt.ylabel("Status quo")
+    plt.title("Scatter Plot: Age vs Status quo")
+    plt.grid(True)
+    plt.show()
 
-plt.scatter(aligned_df["age"], aligned_df["income"], alpha=0.6, color='purple')
-plt.xlabel("Age")
-plt.ylabel("Income")
-plt.title("Scatter Plot: Age vs Income")
-plt.grid(True)
-plt.show()
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/chile.csv")
+    exercise_1(df)
+    exercise_2(df)
+    exercise_3(df)
